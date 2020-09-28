94fba7fd054feff35d67518c8cf934f354bccf2a
Rakitzis45 <michael.rakitzis@gmail.com>
2020-09-28 10:39:56 -0400

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,16 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def greeting 
+10.times {
+    puts "Hello World!"
+}
+end
+ greeting
+
+ def greeting_loop
+    counter = 0
+    until counter == 10
+        puts "Hello World!"
+        counter += 1
+    end
+ end
